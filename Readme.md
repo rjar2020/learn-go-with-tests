4badb3c5205d0a329b30cc93091c9ec55cf19a31
Rolando Angelini <rjar2020@gmai.com>
2021-02-08 10:12:11 +0100

    Including iteration package

--- Readme.md
+++ Readme.md
@@ -11,4 +11,11 @@ From your CI or the terminal for executing the developer tests in a particular p
 go test github.com/rjar2020/learn-go-with-tests/ints
 ```
 
+## Benchmarks
+From your CI or the terminal for executing the developer tests in a particular package execute the following:
+
+```bash
+go test -bench=. github.com/rjar2020/learn-go-with-tests/iteration
+```
+
 [Learn Go with Tests by Chris James]: https://quii.gitbook.io/learn-go-with-tests/
---
+++ iteration/repeat_test.go
@@ -0,0 +1,52 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, repeated, expected string) {
+		t.Helper()
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	}
+
+	t.Run("Repeat a character, 10 times", func(t *testing.T) {
+		repeated := Repeat("a", 10)
+		expected := "aaaaaaaaaa"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat a character, pasing a negative multiplier", func(t *testing.T) {
+		repeated := Repeat("z", -1)
+		expected := "z"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat a character, 0 times", func(t *testing.T) {
+		repeated := Repeat("b", 0)
+		expected := "b"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat a character, 1 time", func(t *testing.T) {
+		repeated := Repeat("W", 1)
+		expected := "W"
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
