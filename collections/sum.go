42cf33f2da424bae2c1883385fe44d4970f9d599
Rolando Angelini <rjar2020@gmai.com>
2021-02-11 18:20:20 +0100

    Collections Take 2: deeper on slices

--- collections/sum.go
+++ collections/sum.go
@@ -8,3 +8,18 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+//SumAllTails sums up all the numbers in the input slices
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
