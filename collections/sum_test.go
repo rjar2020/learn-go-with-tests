a09e1e4f530576de6f2e27011b863ac43040a965
Rolando Angelini <rjar2020@gmail.com>
2021-02-12 16:22:04 +0100

    Collections Take 2: deeper on slices (#6)
    Collections Take 2: deeper on slices

--- collections/sum_test.go
+++ collections/sum_test.go
@@ -1,23 +1,67 @@
 package collections
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	assertCorrectResult := func(t testing.TB, numbers []int, got, want int) {
+	numbers := []int{1, 2, 3}
+
+	got := Sum(numbers)
+	want := 6
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]int{1, 2, 3})
+	}
+}
+
+func ExampleSum() {
+	numbers := []int{1, 2, 3, 10, 1000}
+	fmt.Println(Sum(numbers))
+	// Output: 1016
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checksum := func(t testing.TB, got, want []int) {
 		t.Helper()
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	}
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		assertCorrectResult(t, numbers, got, want)
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checksum(t, got, want)
 	})
 
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checksum(t, got, want)
+	})
+
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([]int{1, 2, 3}, []int{0, 9})
+	}
+}
+
+func ExampleSumAllTails() {
+	got := SumAllTails([]int{}, []int{3, 4, 5}, []int{1, 2, 3, 10, 1000})
+	fmt.Println(got)
+	// Output: [0 9 1015]
 }
