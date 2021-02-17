251fc6a60709fd9142b865c5c4af124e07ad46f1
Rolando Angelini <rjar2020@gmai.com>
2021-02-17 09:56:11 +0100

    Structs part I: the basics + testify package - improving tests messages

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -10,14 +10,14 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-	assert.Equal(t, want, got, "they should be equal")
+	assert.Equal(t, want, got, "The rectangle doesn't have the expected perimeter")
 }
 
 func TestArea(t *testing.T) {
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
-		assert.Equal(t, want, got, "they should be equal")
+		assert.Equal(t, want, got, "The shape doesn't have the expected area")
 	}
 
 	t.Run("rectangles", func(t *testing.T) {
