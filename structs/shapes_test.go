c096bc86dc64baa3c653819c1a707f4527857347
Rolando Angelini <rjar2020@gmail.com>
2021-03-18 10:29:53 +0100

    Structs - take II: table driven tests (#11)
    * Structs - take II: table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,33 +1,149 @@
 package structs
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := rectangle.Perimeter()
-	want := 40.0
-	assert.Equal(t, want, got, "The rectangle doesn't have the expected perimeter")
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			require.Equal(t, tt.hasArea, got, "The shape doesn't have the expected area")
+		})
+	}
 }
 
-func TestArea(t *testing.T) {
+func TestPerimeter(t *testing.T) {
+	areaTests := []struct {
+		name         string
+		shape        PolygonalShape
+		hasPerimeter float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasPerimeter: 36},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasPerimeter: 28.97056274847714},
+	}
 
-	assert := require.New(t)
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			require.Equal(t, tt.hasPerimeter, got, "The shape doesn't have the expected area")
+		})
+	}
+}
 
-	t.Run("calculates a Rectangle's area", func(t *testing.T) {
-		got := Rectangle{12, 6}.Area()
-		want := 72.0
-		assert.Equal(want, got)
-	})
+func TestCircumference(t *testing.T) {
+	areaTests := []struct {
+		name             string
+		shape            EllipseShape
+		hasCircumference float64
+	}{
+		{name: "Circle", shape: Circle{Radius: 10}, hasCircumference: 62.83185307179586},
+	}
 
-	t.Run("calculates a Circles's area", func(t *testing.T) {
-		got := Circle{10}.Area()
-		want := 314.1592653589793
-		assert.Equal(want, got)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Circumference()
+			require.Equal(t, tt.hasCircumference, got, "The shape doesn't have the expected area")
+		})
+	}
+}
 
+func BenchmarkArea(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		areaTests := []struct {
+			name    string
+			shape   Shape
+			hasArea float64
+		}{
+			{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+			{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+			{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		}
+		for _, tt := range areaTests {
+			tt.shape.Area()
+		}
+	}
+}
+
+func BenchmarkPerimeter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		areaTests := []struct {
+			name         string
+			shape        PolygonalShape
+			hasPerimeter float64
+		}{
+			{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasPerimeter: 36},
+			{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasPerimeter: 28.97056274847714},
+		}
+		for _, tt := range areaTests {
+			tt.shape.Perimeter()
+		}
+	}
+}
+
+func BenchmarkCircumference(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		areaTests := []struct {
+			name             string
+			shape            EllipseShape
+			hasCircumference float64
+		}{
+			{name: "Circle", shape: Circle{Radius: 10}, hasCircumference: 62.83185307179586},
+		}
+		for _, tt := range areaTests {
+			tt.shape.Circumference()
+		}
+	}
+}
+func ExampleTriangle_Area() {
+	triangle := Triangle{Base: 12, Height: 6}
+	fmt.Print(triangle.Area())
+	fmt.Print(" ")
+	/* Output: 36*/
+}
+
+func ExampleRectangle_Area() {
+	rectangle := Rectangle{Width: 12, Height: 6}
+	fmt.Print(rectangle.Area())
+	/* Output: 72*/
+}
+
+func ExampleCircle_Area() {
+	circle := Circle{Radius: 10}
+	fmt.Print(circle.Area())
+	fmt.Print(" ")
+	/* Output: 314.1592653589793*/
+}
+
+func ExampleRectangle_Perimeter() {
+	rectangle := Rectangle{Width: 12, Height: 6}
+	fmt.Print(rectangle.Perimeter())
+	/* Output: 36*/
+}
+
+func ExampleTriangle_Perimeter() {
+	triangle := Triangle{Base: 12, Height: 6}
+	fmt.Print(triangle.Perimeter())
+	fmt.Print(" ")
+	/* Output: 28.97056274847714*/
+}
+
+func ExampleCircle_Circumference() {
+	circle := Circle{Radius: 10}
+	fmt.Print(circle.Circumference())
+	fmt.Print(" ")
+	/* Output: 62.83185307179586*/
 }
