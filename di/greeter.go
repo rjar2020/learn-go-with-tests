f9492d65682a872511972cdeb54a215c53c87685
Rolando Angelini <rjar2020@gmail.com>
2021-04-20 09:45:13 +0200

    How does DI work in Golang? (#14)
    * How does DI work in Golang?

---
+++ di/greeter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+//Greet says hello to name
+func Greet(writer io.Writer, name string) {
+	_, _ = fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+//MyGreeterHandler shows how powerful are Go interfaces for DI, using a HTTP server
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+// Excute go run greeter.go and see the outcome in http://localhost:5000
+func main() {
+	Greet(os.Stdout, "Eloide")
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
