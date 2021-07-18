f73fd67bc68cec9b0ab9d9792e0c97b779f90d71
rjar2020 <rjar2020@gmail.com>
2021-07-18 20:25:59 +0200

    It's time for Reflection in Go - more scenarios

--- reflection/walk.go
+++ reflection/walk.go
@@ -5,21 +5,33 @@ import "reflect"
 func Walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
-		for i := 0; i < val.Len(); i++ {
-			Walk(val.Index(i).Interface(), fn)
-		}
-		return
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
 	}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
 }
--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -70,6 +70,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Barcelona"},
 		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Barcelona"},
+			},
+			[]string{"London", "Barcelona"},
+		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
@@ -80,7 +96,42 @@ func TestWalk(t *testing.T) {
 				got = append(got, input)
 			})
 
-			assert.Equal(test.ExpectedCalls, got)
+			assert.ElementsMatch(test.ExpectedCalls, got)
 		})
 	}
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		Walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		assert.Equal(want, got)
+	})
+
+	t.Run("with functions", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		Walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		assert.Equal(want, got)
+	})
 }
