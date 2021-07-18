baf569f8d81717d3fa85a3b580d06ef499cf4591
rjar2020 <rjar2020@gmail.com>
2021-07-18 17:24:04 +0200

    It's time for Reflection in Go - more scenarios

--- reflection/walk.go
+++ reflection/walk.go
@@ -2,8 +2,34 @@ package reflection
 
 import "reflect"
 
-func Walk(x interface{}, fn func(input string))  {
+func Walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			Walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			Walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -6,6 +6,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	assert := require.New(t)
 	cases := []struct {
@@ -19,6 +29,46 @@ func TestWalk(t *testing.T) {
 				Name string
 			}{"Chris"},
 			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Barcelona"},
+			},
+			[]string{"London", "Barcelona"},
 		},
 	}
 
